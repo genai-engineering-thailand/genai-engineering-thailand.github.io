ea6911bd55e5e115ab6b7fba76ae71cb8fe58a74
Chalach Monkhontirapat <driftkao@gmail.com>
2024-07-21 14:23:21 +0700

    docs : deploy your llm with databricks and mlflow

---
+++ docs/knowledge-sharing/7 - Deploy Your LLM with Databricks and MLflow/material/llm-rag-chatbot/01-first-step/chain.py
@@ -0,0 +1,87 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Your langchain RAG chain
+# MAGIC
+# MAGIC In this notebook, we'll put together our langchain application.
+# MAGIC
+# MAGIC It's best practice to build your chain as a separate file and reference it directly to avoid serialization issues. 
+# MAGIC
+# MAGIC <!-- Collect usage data (view). Remove it to disable collection or disable tracker during installation. View README for more details.  -->
+# MAGIC <img width="1px" src="https://ppxrzfxige.execute-api.us-west-2.amazonaws.com/v1/analytics?category=data-science&org_id=4190625599384605&notebook=%2F01-first-step%2Fchain&demo_name=llm-rag-chatbot&event=VIEW&path=%2F_dbdemos%2Fdata-science%2Fllm-rag-chatbot%2F01-first-step%2Fchain&version=1">
+
+# COMMAND ----------
+
+from databricks.vector_search.client import VectorSearchClient
+from langchain_community.vectorstores import DatabricksVectorSearch
+from langchain.schema.runnable import RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+import mlflow
+
+## Enable MLflow Tracing
+mlflow.langchain.autolog()
+
+model_config = mlflow.models.ModelConfig(development_config="rag_chain_config.yaml")
+
+# Connect to the Vector Search Index
+vs_index = VectorSearchClient(disable_notice=True).get_index(
+    endpoint_name=model_config.get("vector_search_endpoint_name"),
+    index_name=model_config.get("vector_search_index"),
+)
+
+# Turn the Vector Search index into a LangChain retriever
+vector_search_as_retriever = DatabricksVectorSearch(
+    vs_index,
+    text_column="content",
+    columns=["id", "content", "url"],
+).as_retriever(search_kwargs={"k": 3}) # Number of search results that the retriever returns
+# Enable the RAG Studio Review App and MLFlow to properly display track and display retrieved chunks for evaluation
+mlflow.models.set_retriever_schema(primary_key="id", text_column="content", doc_uri="url")
+
+# Method to format the docs returned by the retriever into the prompt (keep only the text from chunks)
+def format_context(docs):
+    chunk_contents = [f"Passage: {d.page_content}\n" for d in docs]
+    return "".join(chunk_contents)
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatDatabricks
+from operator import itemgetter
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (  # System prompt contains the instructions
+            "system",
+            """You are an assistant that answers questions. Use the following pieces of retrieved context to answer the question. Some pieces of context may be irrelevant, in which case you should not use them to form the answer.
+
+Context: {context}""",
+        ),
+        # User's question
+        ("user", "{question}"),
+    ]
+)
+
+# Our foundation model answering the final prompt
+model = ChatDatabricks(
+    endpoint=model_config.get("llm_model_serving_endpoint_name"),
+    extra_params={"temperature": 0.01, "max_tokens": 500}
+)
+
+# Return the string contents of the most recent messages: [{...}] from the user to be used as input question
+def extract_user_query_string(chat_messages_array):
+    return chat_messages_array[-1]["content"]
+
+# RAG Chain
+chain = (
+    {
+        "question": itemgetter("messages") | RunnableLambda(extract_user_query_string),
+        "context": itemgetter("messages")
+        | RunnableLambda(extract_user_query_string)
+        | vector_search_as_retriever
+        | RunnableLambda(format_context),
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+# Tell MLflow logging where to find your chain.
+mlflow.models.set_model(model=chain)
---
+++ docs/knowledge-sharing/7 - Deploy Your LLM with Databricks and MLflow/material/llm-rag-chatbot/_resources/00-init-advanced.py
@@ -0,0 +1,98 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC # init notebook setting up the backend. 
+# MAGIC
+# MAGIC Do not edit the notebook, it contains import and helpers for the demo
+# MAGIC
+# MAGIC <!-- Collect usage data (view). Remove it to disable collection or disable tracker during installation. View README for more details.  -->
+# MAGIC <img width="1px" src="https://ppxrzfxige.execute-api.us-west-2.amazonaws.com/v1/analytics?category=data-science&org_id=4190625599384605&notebook=%2F_resources%2F00-init-advanced&demo_name=llm-rag-chatbot&event=VIEW&path=%2F_dbdemos%2Fdata-science%2Fllm-rag-chatbot%2F_resources%2F00-init-advanced&version=1">
+
+# COMMAND ----------
+
+# MAGIC %run ./00-init
+
+# COMMAND ----------
+
+import requests
+import collections
+import os
+ 
+def download_file_from_git(dest, owner, repo, path):
+    def download_file(url, destination):
+      local_filename = url.split('/')[-1]
+      # NOTE the stream=True parameter below
+      with requests.get(url, stream=True) as r:
+          r.raise_for_status()
+          print('saving '+destination+'/'+local_filename)
+          with open(destination+'/'+local_filename, 'wb') as f:
+              for chunk in r.iter_content(chunk_size=8192): 
+                  # If you have chunk encoded response uncomment if
+                  # and set chunk_size parameter to None.
+                  #if chunk: 
+                  f.write(chunk)
+      return local_filename
+
+    if not os.path.exists(dest):
+      os.makedirs(dest)
+    from concurrent.futures import ThreadPoolExecutor
+    files = requests.get(f'https://api.github.com/repos/{owner}/{repo}/contents{path}').json()
+    files = [f['download_url'] for f in files if 'NOTICE' not in f['name']]
+    def download_to_dest(url):
+         download_file(url, dest)
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        collections.deque(executor.map(download_to_dest, files))
+
+# COMMAND ----------
+
+def upload_pdfs_to_volume(volume_path):
+  download_file_from_git(volume_path, "databricks-demos", "dbdemos-dataset", "/llm/databricks-pdf-documentation")
+
+def upload_dataset_to_volume(volume_path):
+  download_file_from_git(volume_path, "databricks-demos", "dbdemos-dataset", "/llm/databricks-documentation")
+
+# COMMAND ----------
+
+def deduplicate_assessments_table(assessment_table):
+    # De-dup response assessments
+    assessments_request_deduplicated_df = spark.sql(f"""select * except(row_number)
+                                        from ( select *, row_number() over (
+                                                partition by request_id
+                                                order by
+                                                timestamp desc
+                                            ) as row_number from {assessment_table} where text_assessment is not NULL
+                                        ) where row_number = 1""")
+    # De-dup the retrieval assessments
+    assessments_retrieval_deduplicated_df = spark.sql(f"""select * except( retrieval_assessment, source, timestamp, text_assessment),
+        any_value(timestamp) as timestamp,
+        any_value(source) as source,
+        collect_list(retrieval_assessment) as retrieval_assessments
+      from {assessment_table} where retrieval_assessment is not NULL group by request_id, source.id, step_id"""    )
+
+    # Merge together
+    assessments_request_deduplicated_df = assessments_request_deduplicated_df.drop("retrieval_assessment", "step_id")
+    assessments_retrieval_deduplicated_df = assessments_retrieval_deduplicated_df.withColumnRenamed("request_id", "request_id2").withColumnRenamed("source", "source2").drop("step_id", "timestamp")
+
+    merged_deduplicated_assessments_df = assessments_request_deduplicated_df.join(
+        assessments_retrieval_deduplicated_df,
+        (assessments_request_deduplicated_df.request_id == assessments_retrieval_deduplicated_df.request_id2) &
+        (assessments_request_deduplicated_df.source.id == assessments_retrieval_deduplicated_df.source2.id),
+        "full"
+    ).select(
+        [str(col) for col in assessments_request_deduplicated_df.columns] +
+        [assessments_retrieval_deduplicated_df.retrieval_assessments]
+    )
+
+    return merged_deduplicated_assessments_df
+
+# COMMAND ----------
+
+# Helper function
+def get_latest_model(model_name):
+    from mlflow.tracking import MlflowClient
+    mlflow_client = MlflowClient(registry_uri="databricks-uc")
+    latest_version = None
+    for mv in mlflow_client.search_model_versions(f"name='{model_name}'"):
+        version_int = int(mv.version)
+        if not latest_version or version_int > int(latest_version.version):
+            latest_version = mv
+    return latest_version
